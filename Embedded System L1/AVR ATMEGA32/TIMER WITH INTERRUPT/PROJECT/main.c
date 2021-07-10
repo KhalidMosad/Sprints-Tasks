238c7bc8076e6e84ee3cd2cce50a6d16ce84ba6a
KhalidMosad <75099681+KhalidMosad@users.noreply.github.com>
2021-07-10 21:55:20 +0200

    TIMER DRIVER

---
+++ Embedded System L1/AVR ATMEGA32/TIMER WITH INTERRUPT/PROJECT/main.c
@@ -0,0 +1,42 @@
+
+#include"STD_TYPES.h"
+#include"BIT_MATH.h"
+#include"DIO_interface.h"
+#include "TIMER_Interface.h"
+
+void Toggle(void)
+{
+	DIO_voidTogglePin(PORTA,PIN0);
+}
+void Toggle1(void)
+{
+	DIO_voidTogglePin(PORTB,PIN0);
+}
+
+void main(void)
+{
+	ST_TIMER_config_t TM0 = {TIMER0, NORMAL,CLK_256, 0,INTERRUPT_ENABLE};
+
+	ST_TIMER_config_t TM1 = {TIMER2, NORMAL,CLK_256, 0,INTERRUPT_ENABLE};
+
+
+    ST_DIO_config_t Led ={PORTA,PIN0,OUTPUT};
+    ST_DIO_config_t Led1 ={PORTB,PIN0,OUTPUT};
+
+
+    DIO_voidSetPinDirection(&Led);
+
+    DIO_voidSetPinDirection(&Led1);
+
+    TM_voidTimerInit(&TM0);
+
+    TM_voidTimerInit(&TM1);
+
+
+    TM0_OV_voidSetCallBack(Toggle);
+
+    TM2_OV_voidSetCallBack(Toggle1);
+
+    while (1);
+
+}
